2b066aca75953160c4246969a6b94120c1ae0d08
dough <viktor.melin@me.com>
2024-01-02 16:05:58 +0100

    Create global types

---
+++ types/globals.d.ts
@@ -0,0 +1,17 @@
+import { LevelWithSilentOrString } from 'pino';
+
+declare global {
+  namespace Express {
+    interface Request {}
+    interface Application {
+      start: (port: number) => any;
+    }
+  }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: number;
+      LOG_LEVEL: LevelWithSilentOrString;
+    }
+  }
+}
