172305f74e3032e701850dd4f865c0f6cebec838
dough <viktor.melin@me.com>
2024-01-03 14:09:52 +0100

    Fix global types

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as express from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
--- src/types/overrides.d.ts
+++ src/types/overrides.d.ts
@@ -1,13 +1,6 @@
 import { LevelWithSilentOrString } from 'pino';
 
 declare global {
-  namespace Express {
-    interface Request {}
-    interface Application {
-      start: (port: number) => any;
-    }
-  }
-
   namespace NodeJS {
     interface ProcessEnv {
       PORT: number;
