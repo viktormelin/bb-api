462b7b0ac315283a1d6964d9bdcdac6db5d1e8d0
viktormelin <viktor.melin@me.com>
2024-01-08 21:43:24 +0100

    Create utils for tokens

--- src/utils/authorizer.ts
+++ src/utils/authorizer.ts
@@ -1,7 +1,23 @@
 import { Authorizer } from '@authorizerdev/authorizer-js';
+import { Request } from 'express';
 
 export const authRef = new Authorizer({
   authorizerURL: 'https://auth.billbuddies.app',
   redirectURL: 'https://billbuddies.app/dashboard',
   clientID: '2df42098-a689-4c65-97db-74ed796e5beb',
 });
+
+export const headersFromToken = (req: Request) => {
+  const headers: Record<string, string> = {
+    Authorization: req.headers.authorization || '',
+  };
+  return headers;
+};
+
+export const getBearerToken = (req: Request) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return null;
+
+  const splitHeader = authHeader.split(' ');
+  return splitHeader[1];
+};
