22c214a406b69e014989d736c6c592b9b5669c11
viktormelin <viktor.melin@me.com>
2024-01-08 21:43:37 +0100

    Create route to validate user JWT

--- src/app/controllers/usersController.ts
+++ src/app/controllers/usersController.ts
@@ -1,12 +1,46 @@
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import expressAsyncHandler from 'express-async-handler';
+import { logger } from '../../utils/logger';
+import {
+  authRef,
+  getBearerToken,
+  headersFromToken,
+} from '../../utils/authorizer';
 
-const getSelf = expressAsyncHandler(async (req: Request) => {
-  console.log(req.user);
+const validateUser = async (req: Request, res: Response) => {
+  const authToken = getBearerToken(req);
 
-  // res.send('Hello World!').status(200);
+  if (!authToken)
+    return res.status(403).json({ error: 'Authorization not found' });
+
+  try {
+    const { data, errors } = await authRef.validateJWTToken({
+      token: authToken,
+      token_type: 'id_token', // This can be access_token, refresh_token
+      // roles: [user] // specify roles that you want to validate jwt for, by default it will just verify jwt.
+    });
+
+    if (errors.length) throw new Error(errors[0].message);
+
+    if (!data?.is_valid)
+      return res.status(403).json({ error: 'Invalid JWT token' });
+
+    return res.status(200);
+  } catch (error) {
+    logger.error(error);
+    return res.status(403).json({ error: 'Invalid JWT token' });
+  }
+};
+
+const getSelf = expressAsyncHandler(async (req: Request, res: Response) => {
+  const { data, errors } = await authRef.getProfile(headersFromToken(req));
+
+  if (errors.length) throw new Error(errors[0].message);
+
+  res.json(data).status(200);
 });
 
 export default {
+  validateUser,
   getSelf,
 };
--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -4,6 +4,7 @@ import authMiddleware from '../../app/middleware/authMiddleware';
 
 const router = Router();
 
+router.get('/validate', authMiddleware, usersController.validateUser);
 router.get('/me', authMiddleware, usersController.getSelf);
 
 export default router;
