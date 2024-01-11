ff614d9cb080871af392746e4b137cd0c1dc55ad
viktormelin <viktor.melin@me.com>
2024-01-11 10:42:48 +0100

    Fix issue with no import defined for type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as express from 'express';
+import { JWTUser } from '../IUser';
 
 declare global {
   namespace Express {
