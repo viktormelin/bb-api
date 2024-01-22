e18cd780e9613964ef304f87ebe885dfd10a0b69
viktormelin <viktor.melin@me.com>
2024-01-22 13:56:24 +0100

    Add routes for joining groups

--- src/routes/v1/groups.ts
+++ src/routes/v1/groups.ts
@@ -6,5 +6,7 @@ const router = Router();
 
 router.get('/', authMiddleware, groupsController.getMyGroups);
 router.post('/new', authMiddleware, groupsController.createNewGroup);
+router.get('/:slug', authMiddleware, groupsController.getGroup);
+router.post('/join/:slug', authMiddleware, groupsController.joinGroup);
 
 export default router;
